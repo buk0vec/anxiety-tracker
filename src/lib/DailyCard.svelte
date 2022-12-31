200efd65ce3ff32b1c98e6bc91ce1d610cb47b49
Nick Buk[0vec] <nbukovec@calpoly.edu>
2022-12-30 18:33:54 -0800

    :bug: date output fix
    Changed the date output to refer to the UTC date rather than the local tz

--- src/lib/DailyCard.svelte
+++ src/lib/DailyCard.svelte
@@ -2,7 +2,9 @@
 	import type { DailyRecord } from '@prisma/client';
 
 	export let record: DailyRecord;
+
+	const date = record.date.toUTCString();
 </script>
 
-<p>Date: {record.date.toDateString()}</p>
+<p>Date: {date.substring(0, date.indexOf(':') - 3)}</p>
 <p>Levels: {record.anxiety_level} {record.depression_level} {record.worry_level}</p>
