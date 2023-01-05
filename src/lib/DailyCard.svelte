e9054e2052352aed816f1285ec1111f7cf3362a2
Nick Buk[0vec] <nbukovec@calpoly.edu>
2023-01-05 14:17:24 -0800

    :lipstick: improve DailyCard styling
    Made the daily report card look decent

--- src/lib/DailyCard.svelte
+++ src/lib/DailyCard.svelte
@@ -3,8 +3,44 @@
 
 	export let record: DailyRecord;
 
+	let show = false;
+
 	const date = record.date.toUTCString();
 </script>
 
-<p>Date: {date.substring(0, date.indexOf(':') - 3)}</p>
-<p>Levels: {record.anxiety_level} {record.depression_level} {record.worry_level}</p>
+<div
+	class="bg-slate-200 rounded p-4 my-2 hover:outline transition-outline cursor-pointer"
+	on:click|self={() => (show = !show)}
+	on:keypress|self={() => (show = !show)}
+>
+	<div
+		class="flex flex-row justify-between cursor-pointer"
+		on:click={() => (show = !show)}
+		on:keypress={() => (show = !show)}
+	>
+		<span class="font-bold inline">{date.substring(0, date.indexOf(':') - 3)}</span>
+
+		<span>
+			{#if !show}
+				Click to expand
+			{:else}
+				Click to hide
+			{/if}
+		</span>
+	</div>
+	{#if show}
+		<div class="cursor-auto inline">
+			<p class="inline">
+				Average anxiety: <span class="font-semibold">{record.anxiety_level}<span /></span>
+			</p>
+			<br />
+			<p class="inline">
+				Average depression: <span class="font-semibold">{record.depression_level}</span>
+			</p>
+			<br />
+			<p class="inline">
+				Average worry about panic: <span class="font-semibold">{record.worry_level}</span>
+			</p>
+		</div>
+	{/if}
+</div>
