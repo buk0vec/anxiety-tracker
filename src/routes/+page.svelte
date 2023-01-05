3132f28ca852cc2b1e45642b71efa1ad40508ce3
Nick Buk[0vec] <nbukovec@calpoly.edu>
2023-01-04 16:17:03 -0800

    :lipstick: checkboxes + form toggle
    Checkboxes are now stacked vertically, panic form can be toggled.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,7 @@
 			data.now.getFullYear() === r.date.getUTCFullYear()
 	);
 
+	let showPanicForm = false;
 	let fearSlider = 0;
 </script>
 
@@ -60,98 +61,131 @@
 	<p>You've already done your daily check-in!</p>
 	<button
 		class="bg-slate-100 p-2 rounded border-2 border-purple-400 hover:bg-purple-400 transition-colors"
-		on:click={() => (showDaily = !showDaily)}>Show form</button
+		on:click={() => (showDaily = !showDaily)}>Show check-in form</button
 	>
 {/if}
 <h2 class="text-xl mt-4">Panic Attack Reporting</h2>
-<form method="POST" action="?/createPanic" class="flex flex-col">
-	<label for="panic_date">Date</label>
-	<input
-		type="date"
-		name="date"
-		id="panic_date"
-		value={`${data.now.getFullYear()}-${data.now.getMonth() < 9 ? '0' : ''}${
-			data.now.getMonth() + 1
-		}-${data.now.getDate() < 10 ? '0' : ''}${data.now.getDate()}`}
-		required
-	/>
-	<label for="panic_time">Time</label>
-	<input type="time" name="time" id="panic_time" required />
-	<label for="panic_triggers">Triggers</label>
-	<input
-		type="text"
-		name="triggers"
-		id="panic_triggers"
-		placeholder="What situation triggered the attack?"
-		required
-	/>
-	<fieldset>
-		<input type="radio" id="panic_expected" name="expected" value="expected" required />
-		<label for="panic_expected">Expected</label>
-		<input type="radio" id="panic_unexpected" name="expected" value="unexpected" />
-		<label for="panic_unexpected">Unexpected</label>
-	</fieldset>
-	<label for="panic_max_fear">Maximum Fear ({fearSlider})</label>
-	<input
-		type="range"
-		id="panic_max_fear"
-		name="max_fear"
-		min="0"
-		max="10"
-		required
-		bind:value={fearSlider}
-	/>
-	<p>Check all symptoms present to at least a mild degree:</p>
-	<fieldset>
-		<input type="checkbox" id="panic_chest_pain" name="symptoms" value="chest_pain" />
-		<label for="panic_chest_pain">Chest pain</label>
-		<input type="checkbox" id="panic_sweating" name="symptoms" value="sweating" />
-		<label for="panic_sweating">Sweating</label>
-		<input type="checkbox" id="panic_heart_pounding" name="symptoms" value="heart_pounding" />
-		<label for="panic_heart_pounding">Heart racing/palpitations/pounding</label>
-		<input type="checkbox" id="panic_nausea" name="symptoms" value="nausea" />
-		<label for="panic_nausea">Nausea/upset stomach</label>
-		<input type="checkbox" id="panic_short_breath" name="symptoms" value="short_breath" />
-		<label for="panic_short_breath">Shortness of breath</label>
-		<input type="checkbox" id="panic_dizzy" name="symptoms" value="dizzy" />
-		<label for="panic_dizzy">Dizzy/unsteady/lightheaded/faint</label>
-		<input type="checkbox" id="panic_shaking" name="symptoms" value="shaking" />
-		<label for="panic_shaking">Shaking/trembling</label>
-		<input type="checkbox" id="panic_chills_flushes" name="symptoms" value="chills_flushes" />
-		<label for="panic_chills_flushes">Chills/hot flushes</label>
-		<input type="checkbox" id="panic_numbness" name="symptoms" value="numbness" />
-		<label for="panic_numbness">Numbness/tingling</label>
-		<input type="checkbox" id="panic_unreality" name="symptoms" value="unreality" />
-		<label for="panic_unreality">Feelings of unreality</label>
-		<input type="checkbox" id="panic_choking" name="symptoms" value="choking" />
-		<label for="panic_choking">Feelings of choking</label>
-		<input type="checkbox" id="panic_dying" name="symptoms" value="dying" />
-		<label for="panic_dying">Fear of dying</label>
-		<input type="checkbox" id="panic_losing_control" name="symptoms" value="losing_control" />
-		<label for="panic_losing_control">Fear of losing control/going insane</label>
-	</fieldset>
-	<label for="panic_thoughts">Thoughts</label>
-	<input
-		type="text"
-		id="panic_thoughts"
-		name="thoughts"
-		placeholder="Your thoughts in the moment"
-		required
-	/>
-	<label for="panic_behaviors">Behaviors</label>
-	<input
-		type="text"
-		id="panic_behaviors"
-		name="behaviors"
-		placeholder="What did you do during/after?"
-		required
-	/>
-	<button
-		class="bg-slate-100 rounded my-4 p-2 border-solid border-2 border-purple-400 hover:bg-purple-400 transition-colors"
-		>Submit</button
-	>
-</form>
-<h3 class="text-lg">Past records</h3>
+<button
+	class="bg-slate-100 p-2 rounded border-2 border-purple-400 hover:bg-purple-400 transition-colors"
+	on:click={() => (showPanicForm = !showPanicForm)}
+	>{showPanicForm ? 'Hide' : 'Show'} panic report form</button
+>
+{#if showPanicForm}
+	<form method="POST" action="?/createPanic" class="flex flex-col">
+		<label for="panic_date">Date</label>
+		<input
+			type="date"
+			name="date"
+			id="panic_date"
+			value={`${data.now.getFullYear()}-${data.now.getMonth() < 9 ? '0' : ''}${
+				data.now.getMonth() + 1
+			}-${data.now.getDate() < 10 ? '0' : ''}${data.now.getDate()}`}
+			required
+		/>
+		<label for="panic_time">Time</label>
+		<input type="time" name="time" id="panic_time" required />
+		<label for="panic_triggers">Triggers</label>
+		<input
+			type="text"
+			name="triggers"
+			id="panic_triggers"
+			placeholder="What situation triggered the attack?"
+			required
+		/>
+		<fieldset>
+			<input type="radio" id="panic_expected" name="expected" value="expected" required />
+			<label for="panic_expected">Expected</label>
+			<input type="radio" id="panic_unexpected" name="expected" value="unexpected" />
+			<label for="panic_unexpected">Unexpected</label>
+		</fieldset>
+		<label for="panic_max_fear">Maximum Fear ({fearSlider})</label>
+		<input
+			type="range"
+			id="panic_max_fear"
+			name="max_fear"
+			min="0"
+			max="10"
+			required
+			bind:value={fearSlider}
+		/>
+		<p>Check all symptoms present to at least a mild degree:</p>
+		<fieldset class="flex flex-col">
+			<span>
+				<input type="checkbox" id="panic_chest_pain" name="symptoms" value="chest_pain" />
+				<label for="panic_chest_pain">Chest pain</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_sweating" name="symptoms" value="sweating" />
+				<label for="panic_sweating">Sweating</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_heart_pounding" name="symptoms" value="heart_pounding" />
+				<label for="panic_heart_pounding">Heart racing/palpitations/pounding</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_nausea" name="symptoms" value="nausea" />
+				<label for="panic_nausea">Nausea/upset stomach</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_short_breath" name="symptoms" value="short_breath" />
+				<label for="panic_short_breath">Shortness of breath</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_dizzy" name="symptoms" value="dizzy" />
+				<label for="panic_dizzy">Dizzy/unsteady/lightheaded/faint</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_shaking" name="symptoms" value="shaking" />
+				<label for="panic_shaking">Shaking/trembling</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_chills_flushes" name="symptoms" value="chills_flushes" />
+				<label for="panic_chills_flushes">Chills/hot flushes</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_numbness" name="symptoms" value="numbness" />
+				<label for="panic_numbness">Numbness/tingling</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_unreality" name="symptoms" value="unreality" />
+				<label for="panic_unreality">Feelings of unreality</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_choking" name="symptoms" value="choking" />
+				<label for="panic_choking">Feelings of choking</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_dying" name="symptoms" value="dying" />
+				<label for="panic_dying">Fear of dying</label>
+			</span>
+			<span>
+				<input type="checkbox" id="panic_losing_control" name="symptoms" value="losing_control" />
+				<label for="panic_losing_control">Fear of losing control/going insane</label>
+			</span>
+		</fieldset>
+		<label for="panic_thoughts">Thoughts</label>
+		<input
+			type="text"
+			id="panic_thoughts"
+			name="thoughts"
+			placeholder="Your thoughts in the moment"
+			required
+		/>
+		<label for="panic_behaviors">Behaviors</label>
+		<input
+			type="text"
+			id="panic_behaviors"
+			name="behaviors"
+			placeholder="What did you do during/after?"
+			required
+		/>
+		<button
+			class="bg-slate-100 rounded my-4 p-2 border-solid border-2 border-purple-400 hover:bg-purple-400 transition-colors"
+			>Submit</button
+		>
+	</form>
+{/if}
+<h3 class="text-lg">Past check-ins</h3>
 {#each [...data.daily].sort((a, b) => b.date.getTime() - a.date.getTime()) as record}
 	<DailyCard {record} />
 {/each}
