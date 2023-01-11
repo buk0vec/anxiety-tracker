c02380d9e0b3bf4339b508fe74cecb65f69f81ed
Nick Buk[0vec] <nbukovec@calpoly.edu>
2023-01-10 18:37:16 -0800

    :sparkles: add footer
    Created a basic footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
 	import '../app.postcss';
 	import { page } from '$app/stores';
+	import GithubLink from '$lib/GithubLink.svelte';
 </script>
 
-<div class="p-4">
+<div class="p-4 flex flex-col min-h-screen">
 	<header class="flex flex-row items-baseline gap-4 justify-between sm:pr-6">
 		<h1 class="text-3xl">Anxiety Record</h1>
 		<div class="flex flex-row gap-4 content-end">
@@ -17,5 +18,11 @@
 			{/if}
 		</div>
 	</header>
-	<slot />
+	<div class="flex-1">
+		<slot />
+	</div>
+	<footer class="flex flex-row justify-between items-end pt-4">
+		<p class="text-slate-500">By Nick Bukovec (@buk0vec)</p>
+		<GithubLink />
+	</footer>
 </div>
